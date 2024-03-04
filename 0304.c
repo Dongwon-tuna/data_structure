c5388564140060d3d9873749399bbb7b363c6311
Dongwon-tuna <61178312+Dongwon-tuna@users.noreply.github.com>
2024-03-04 21:30:58 +0900

    0304 my code

--- 0304.c
+++ 0304.c
@@ -1,5 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-int main(void){
-    printf("hello data structure");
+void main(){
+    srand(time(NULL));
+    int num;
+    printf("number:");
+    scanf("%d",&num);
+    int parr[6] = {0,};
+    for (int i = 0; i < num; i++)
+    {
+        int random = rand() % 6+1;
+        parr[random-1]= parr[random-1] + 1;
+        printf("%d\n",random);
+
+    }
+    for (int i = 0; i < 6; i++)
+    {
+        printf("%d: %d\n",i+1, parr[i]);
+    }
+    
+    //find most frequent number...> algorithm need.
 }
