43260e867aea95c1304dc7afbbc5fa5a9f0ba701
Dongwon Hwang <61178312+Dongwon-tuna@users.noreply.github.com>
2024-03-25 00:56:12 +0900

    Update 0311.c

--- Week2/0311.c
+++ Week2/0311.c
@@ -1,1 +1,17 @@
 //22차원 배열 포인터 관점으로
+#include <stdio.h>
+#include <stdlib.h>
+int *add(int x, int y);
+void main(){
+    int *res;
+    res = add(5,9);
+    printf("res: %d",*res);
+}
+
+int *add(int x, int y){
+    int *z;
+    z = (int*)malloc(sizeof(int));
+    *z = x+y;
+    return z;
+    
+}
